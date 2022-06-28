780d576feb8c8ae3ea011ecf526e842b9a033bda
Maurice Kyalo <mrckyalo@gmail.com>
2022-06-28 23:41:28 +0300

    Fixed Pycodestyle validation

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-'''
-Checks for lowercase character
-Returns True if c is lowercase
-Returns False otherwise
-'''
+
 def islower(c):
+    '''
+    Checks for lowercase character
+    Returns True if c is lowercase
+    Returns False otherwise
+    '''
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-# Print a string in uppercase.
+
 def uppercase(str):
+    """Print a string in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
