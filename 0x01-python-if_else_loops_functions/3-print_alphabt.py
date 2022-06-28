3da1d018fce1757315e6d3fc49b1cd4391772e6f
Maurice Kyalo <mrckyalo@gmail.com>
2022-06-29 00:01:45 +0300

    Changed ambiguous variable name l

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 
 # Print the alphabet in lowercase, not followed by a new line,
 # except q and e.
-for l in range(97, 123):
-    if chr(l) is not 'q' and chr(l) is not 'e':
-        print("{}".format(chr(l)), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
