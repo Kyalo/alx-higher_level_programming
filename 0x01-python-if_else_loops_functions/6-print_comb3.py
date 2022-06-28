20e8cfc71a810b0dc23b6514325f3b4a21bed306
Maurice Kyalo <mrckyalo@gmail.com>
2022-06-28 23:49:53 +0300

    Fixed string formatting to use .format()

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -7,6 +7,6 @@ The two digits must be different - 01 and 10 are considered identical.
 for x in range(0, 10):
     for y in range(x + 1, 10):
         if x == 8 and y == 9:
-            print(f"{x}{y}")
+            print("{x}{y}".format(x, y))
         else:
-            print(f"{x}{y}", end=", ")
+            print("{x}{y}".format(x, y), end=", ")
