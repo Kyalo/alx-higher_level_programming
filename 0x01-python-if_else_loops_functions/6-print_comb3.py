e070f5054e855a63c8416bd5e4e82c7dccdfcac9
Maurice Kyalo <mrckyalo@gmail.com>
2022-06-28 23:53:12 +0300

    Fixed string formatting to use .format()

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,9 +4,9 @@
 Print all possible different combinations of two digits in ascending order.
 The two digits must be different - 01 and 10 are considered identical.
 """
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print("{x}{y}".format(x, y))
+for digit_1 in range(0, 10):
+    for digit_2 in range(digit_1 + 1, 10):
+        if digit_1 == 8 and digit_2 == 9:
+            print("{}{}".format(digit_1, digit_2))
         else:
-            print("{x}{y}".format(x, y), end=", ")
+            print("{}{}".format(digit_1, digit_2), end=", ")
