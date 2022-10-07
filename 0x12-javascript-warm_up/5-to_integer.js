6ce240d7a8169b231bf9d6ab95eb8b9fa3fe76ef
Maurice Kyalo <mrckyalo@gmail.com>
2022-10-07 11:36:59 +0000

    Improve algorithm to catch edge cases

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+console.log(isNaN(parseInt(process.argv[2])) ? 'Not a number' : 'My number: ' + parseInt(process.argv[2]));
